ae697f968ec1d21068cb70d034845ac9e7449cb7
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-10-12 16:37:21 -0600

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   # This one line gave us a new article path, a post to create articles, 
   # a patch to edit articles, a path to delete articles, show article path, index article path
   resources :articles
+
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 end
